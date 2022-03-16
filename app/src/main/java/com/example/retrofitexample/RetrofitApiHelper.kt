de06eadf64376122c427e6778fa9aca134d66c74
nowxel <nowxel@gmail.com>
2022-03-16 23:49:14 +0200

    feat: retrofitApiHelper added

---
+++ app/src/main/java/com/example/retrofitexample/RetrofitApiHelper.kt
@@ -0,0 +1,26 @@
+package com.example.retrofitexample
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitApiHelper {
+
+    fun getRetrofitInstance(): Retrofit {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client: OkHttpClient = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://api.jsonbin.io/")
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        return retrofit
+    }
+}
--- app/src/main/java/com/example/retrofitexample/TestApiService.kt
+++ app/src/main/java/com/example/retrofitexample/TestApiService.kt
@@ -1,36 +1,19 @@
 package com.example.retrofitexample
 
-import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
 
 class TestApiService {
-    private var api: TestAPI
     private val secretKey: String = "\$2b\$10\$/nbUpxSofW6UgvmjYVbAbeyh.TNMS8QdmITrxAStZor/cNK7JbqUK"
 
-    init {
-        val logging = HttpLoggingInterceptor()
-        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
-        val client: OkHttpClient = OkHttpClient.Builder()
-            .addInterceptor(logging)
-            .build()
-        val retrofit = Retrofit.Builder()
-            .baseUrl("https://api.jsonbin.io/")
-            .client(client)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-        api = retrofit.create(TestAPI::class.java)
-    }
+    private val retrofit: Retrofit = RetrofitApiHelper.getRetrofitInstance()
+    private val api: TestAPI = retrofit.create(TestAPI::class.java)
 
     fun getLocalNews(callback: NewsCallback) {
         api.getLocalNews(secretKey).enqueue(object : Callback<News> {
-            override fun onResponse(call: Call<News>, response:
-            Response<News>
-            ) {
+            override fun onResponse(call: Call<News>, response: Response<News>) {
                 if (response.code() == 200 && response.body() != null)
                     callback.onSuccess(response.body()!!)
                 else
@@ -46,6 +29,4 @@ class TestApiService {
         fun onSuccess(news: News)
         fun onFailure()
     }
-
-
 }
